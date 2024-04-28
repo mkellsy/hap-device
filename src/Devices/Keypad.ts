f61d35671b7e13372ff14622f7dbc426a8c2fd40
Michael Kellsy <mkellsy@amazon.com>
2024-04-28 16:13:57 -0700

    add buttons

--- src/Devices/Keypad.ts
+++ src/Devices/Keypad.ts
@@ -1,6 +1,9 @@
+import { Button } from "./Button";
 import { Common } from "./Common";
 
 export interface Keypad extends Common {
+    buttons: Button[];
+
     update(_status: unknown): void;
     set(_status: unknown): void;
 }
--- src/Devices/Remote.ts
+++ src/Devices/Remote.ts
@@ -1,6 +1,9 @@
+import { Button } from "./Button";
 import { Common } from "./Common";
 
 export interface Remote extends Common {
+    buttons: Button[];
+
     update(_status: unknown): void;
     set(_state: unknown): void;
 }
