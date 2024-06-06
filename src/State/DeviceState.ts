d1ebb087874ef841041e61bebbbb3fd4f47cda51
Michael Kellsy <mkellsy@gmail.com>
2024-06-05 19:58:47 -0700

    remove unessessary extra properties from device state

--- src/State/DeviceState.ts
+++ src/State/DeviceState.ts
@@ -5,14 +5,4 @@ import { Address } from "../Properties/Address";
  */
 export interface DeviceState {
     state: "On" | "Off" | "Open" | "Closed" | "Heat" | "Cool" | "Auto" | "Occupied" | "Unoccupied" | "Unknown";
-    led?: Address;
-    speed?: number;
-    level?: number;
-    tilt?: number;
-    hue?: number;
-    humidity?: number;
-    luminance?: number;
-    temprature?: number;
-    eco?: "On" | "Off";
-    whoosh?: "On" | "Off";
 }
